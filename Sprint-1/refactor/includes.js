dab41881cb3426050d8a1053b4d62d176b1a1509
Sarah Almadi <sarah666lilith@gmail.com>
2026-04-06 09:13:41 +0200

    Refactor The Implementation
    to use a for...of loop

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,13 +1,12 @@
 // Refactor the implementation of includes to use a for...of loop
 
 function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
+  for (const element of list) {
     if (element === target) {
       return true;
     }
   }
   return false;
 }
-
+console.log(includes([2.9, 3, 5], 3)); // should return true, because 3 is an element of the array
 module.exports = includes;
