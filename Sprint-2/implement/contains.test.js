5718408fac31ebdcdbbeb750a3503ecadd3a5360
Sarah Almadi <sarah666lilith@gmail.com>
2026-04-18 10:06:10 +0200

    Implement Contains Function
    To check Specific Properties Inside The Object

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -1,4 +1,6 @@
-const contains = require("./contains.js");
+// const contains = require("./contains.js");
+
+const contains = require("./contains");
 
 /*
 Implement a function called contains that checks an object contains a
@@ -20,16 +22,29 @@ as the object doesn't contains a key of 'c'
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+
+test("contains an empty object returns false", () => {
+  expect(contains({}, "a")).toBe(false);
+});
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
 
+test("Given an object with existing property name, return true", () => {
+  expect(contains({ a: 1, b: 2 }, "a")).toBe(true);
+});
+
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
+test("Given an object with a non-existent property name, return false", () => {
+  expect(contains({ a: 1, b: 2 }, "c")).toBe(false);
+});
 
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+test("Given an object with invalid properties like array, throw error", () => {
+  expect(contains([1, 2, 5])).toThrow("Error", "Invalid parameter");
+});
